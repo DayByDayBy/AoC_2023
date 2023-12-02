fca6c2ff8017c832ff2affbf610d574e706358ec
Diomedes Git <diomedesgit@gmail.com>
2023-12-02 00:01:54 +0000

    deleted wrong bit before

--- day01/new.py
+++ day01/new.py
@@ -7,13 +7,10 @@ def digit_finder():
     with open('day01input.txt', 'r') as data:
         for line in data:
             forward = re.finditer('\d|one|two|three|four|five|six|seven|eight|nine', line)
-            reversed = re.finditer('\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line[::-1])
-            first = 
-            last = 
-         
-    
-            
-            coordinate = (first + last)
+            reverse = re.finditer('\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line[::-1])
+            first = str(next(forward, None))
+            last = str(next(reverse, None))
+            coordinate = int(first + last)
             coordinates.append(coordinate)
         return coordinates
 print(digit_finder())
