8852116b3382fd93f568410fc52a89dbe72d2377
Diomedes Git <diomedesgit@gmail.com>
2023-12-04 22:17:22 +0000

    first chunk done, catching up one today

--- 04day04/scratchcards1.py
+++ 04day04/scratchcards1.py
@@ -1,4 +1,36 @@
+import re 
+
+class Card():
+    def __init__ (self, id, win_nums, play_nums):
+        self.id = id
+        self.win_nums = win_nums
+        self.play_nums = play_nums
+    
 def scratchy():
+    cards = []
     with open('cards.txt', 'r') as data:
         for line in data:
+            card = re.split(r'[:|]', line)
+            id = card[0].strip()
+            win_nums = tuple(map(int, card[1].split()))
+            play_nums = tuple(map(int, card[2].split()))
+            card = Card(id, win_nums, play_nums)
+            cards.append(card)
+
+    return cards             
+card_list = scratchy()
+for card in card_list:
+    print(f"{card.id} - W: {card.win_nums} P: {card.play_nums}")
             
+            
+            
+            
+            #      id = card_data[1].strip()
+            # win_nums = tuple(map(int, data[2].split()))
+            # player_nums = tuple(map(int, data[3].split()))     
+            
+            
+            
+            # game_ID = int(game_info[0].split()[-1])
+            # rounds = game_info[1].split("; ")
+            # possible = True
