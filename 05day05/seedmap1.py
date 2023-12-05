8a7e0a959cccfc1f07c2d1b4a99448dfe7cc8a65
Diomedes Git <diomedesgit@gmail.com>
2023-12-05 23:09:27 +0000

    going old school, that was getting comlicated. may return to the one-liner style once i have the logic sorted

--- 05day05/seedmap1.py
+++ 05day05/seedmap1.py
@@ -1,3 +1,21 @@
-seeds = [[1] + [numbers.split() for numbers in line.split()] for line in open(r"smallmanac.txt")]
+# # seeds = [[1] + [numbers.split() for numbers in line.split()] for line in open(r"smallmanac.txt")]
+# seeds = [[int(num) for num in line.split()] for line in open("smallmanac.txt")]
 
-print(f"{seeds}")
+# print(f"{seeds}")
+
+def process_data(file_path):
+    data_dict = {}
+    current_section = None
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.endswith('map:'):
+                current_section = line.replace(' map:', '')
+                data_dict[current_section] = []
+            elif current_section is not None:
+                data_list = list(map(int, line.split()))
+                data_dict[current_section].append(data_list)
+    for key, value in data_dict.items():
+        print(f"{key}: {value}")
+
+process_data('smallmanac.txt')
