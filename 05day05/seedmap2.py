7b70fc9dd923b2da6ebd4077c26ce9551398f09d
Diomedes Git <diomedesgit@gmail.com>
2023-12-07 16:19:01 +0000

    adding the code. i think it works,  but tbh the process is so huge that the laptop is still running it

--- 05day05/seedmap2.py
+++ 05day05/seedmap2.py
@@ -3,9 +3,16 @@ from functools import reduce
 
 def seedmap(data_link):
     data = open(data_link).read()
-    seeds, *mappings = data.split('\n\n')
-    seeds = list(map(int, seeds.split()[1:]))
-
+    seed_list, *mappings = data.split('\n\n')
+    seeds = list(map(int, seed_list.split()[1:]))
+    # all_seeds = [num for start, end in zip(seeds[::1], seeds[1::2]) for num in range(start, end + 1)]
+    all_seeds = []
+    for i in range(0, len(seeds), 2):
+        seed_start = seeds[i]
+        seed_range = seeds[i+1]
+        spread = range(seed_start, seed_start+seed_range)     
+        all_seeds.extend(spread)
+        
     def findmin(start, mapping):
         for m in mapping.split('\n')[1:]:
             destination, source, length = map(int, m.split())
@@ -18,10 +25,14 @@ def seedmap(data_link):
             # print(f"{mapping}, {start}, {start}")
             return start
 
-    final_locations = [reduce(findmin, mappings, seed) for seed in seeds]
+    final_locations = [reduce(findmin, mappings, seed) for seed in all_seeds]
     min_result = min(final_locations)
 
     print(f"minimum: {min_result}")
 
-seedmap('smallmanac.txt')
+# seedmap('smallmanac.txt')
 seedmap('almanac.txt')
+
+
+
+
