df4d696026cbcfa8f27c412c00d6768edaa49798
Diomedes Git <diomedesgit@gmail.com>
2024-01-03 21:23:25 +0000

    safety commit

--- 09day09/oasis_extrapolator.py
+++ 09day09/oasis_extrapolator.py
@@ -1,20 +1,14 @@
 # # differences
 # # difference between the differences
-# # 
-# # then extrapolate
-# # 
-# # eg if the sequernce is going up by 1 2 3, the difference-difference is 1, and the difference-difference-difference is 0
-# # 
-# # go until zero line, then extrapolate ending to sequence
+# # then extrapolate 
+# # ie if the sequence is going up by 1 2 3, the difference-difference is 1, and the difference-difference-difference is 0
+# # go until zero, then extrapolate ending to sequence, and differeence sequences 
 # # 
 # # line2.position1 = line1.position2-line1.position1
 # # 
 # # 
 
-
-
-
-
+####   --------------------------------------------------------------------------------  ###
 
 # # quick logic sketch:
 
--- 12day12/springs.py
+++ 12day12/springs.py
@@ -12,7 +12,7 @@
             It only takes you a few more steps to reach the main gate of the massive fenced-off area containing the springs. You go through the gate and into a small administrative building.
 
             "Hello! What brings you to the hot springs today? Sorry they're not very hot right now; we're having a lava shortage at the moment." You ask about the missing machine parts for Desert Island.
-
+            
             "Oh, all of Gear Island is currently offline! Nothing is being manufactured at the moment, not until we get more lava to heat our forges. And our springs. The springs aren't very springy unless they're hot!"
 
             "Say, could you go up and see why the lava stopped flowing? The springs are too cold for normal operation, but we should be able to find one springy enough to launch you up there!"
